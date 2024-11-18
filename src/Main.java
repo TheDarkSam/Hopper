7bafd4beba54bd614578a016e673c74cf92b6731
TheDarkSam <samuel_soadsp@outlook.com>
2024-11-18 15:32:06 -0300

    Adicionei um novo aluno

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] por) {
 
         Aluno a1 = new Aluno("Jessica", 20, 9999, 12345);
+        Aluno a2 = new Aluno("Jessica", 20, 9999, 12345);
 
 
     }
